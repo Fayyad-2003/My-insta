cc174709cc24bedf9bc157313ede33937c3a99e5
fayad sh <fayadsh33.fs@gmail.com>
2026-03-05 02:52:12 +0300

    feat(reel): add commenting functionality
    - Create reel_comments table to store comments for reels.
    - Include reel_id, user_id, text, and parent_comment_id for nested comments.
    - Establish foreign key constraints for reel_id and user_id with cascade on delete.
    - Define ReelComment Eloquent model with fillable attributes and user and reel relationships.
    - Enable users to add comments and replies to reels, enhancing user interaction.

--- backend/app/Models/Reel/ReelComment.php
+++ backend/app/Models/Reel/ReelComment.php
@@ -2,9 +2,26 @@
 
 namespace App\Models\Reel;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class ReelComment extends Model
 {
     //
+    protected $fillable = [
+        'reel_id',
+        'user_id',
+        'text',
+        'parent_comment_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reel()
+    {
+        return $this->belongsTo(Reel::class);
+    }
 }
--- backend/database/migrations/2026_03_03_195523_create_reel_comments_table.php
+++ backend/database/migrations/2026_03_03_195523_create_reel_comments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('reel_comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('reel_id')->constrained('reels')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->text('text');
+            $table->foreignId('parent_comment_id')->nullable()->constrained('reel_comments')->cascadeOnDelete();
             $table->timestamps();
         });
     }
