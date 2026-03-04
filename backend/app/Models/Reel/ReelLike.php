e6c3186b54c09f57bd99ee47e73feb327fc105c9
fayad sh <fayadsh33.fs@gmail.com>
2026-03-05 02:55:18 +0300

    feat(reel-likes): add model and migration for reel likes
    - Introduce ReelLike model to manage likes on reels.
    - Define reel_likes table with foreign keys for reel_id and liker_id.
    - Establish belongsTo relationships in the ReelLike model for Reel and User.
    - This lays the groundwork for users to like reels and track these interactions.

--- backend/app/Models/Reel/ReelLike.php
+++ backend/app/Models/Reel/ReelLike.php
@@ -2,9 +2,24 @@
 
 namespace App\Models\Reel;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class ReelLike extends Model
 {
     //
+    protected $fillable = [
+        'liker_id',
+        'reel_id',
+    ];
+
+    public function reel()
+    {
+        return $this->belongsTo(Reel::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
