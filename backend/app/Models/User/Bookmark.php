53db9a1f77177085974aa92bbf74eb5ee2adcfa5
fayad sh <fayadsh33.fs@gmail.com>
2026-03-05 23:54:01 +0300

    feat(user): implement Bookmark model and migration schema
    - add fillable fields: user_id, target_type, target_id
    - define belongsTo relationship with User model
    - add foreign key, target_type, and target_id columns to bookmarks table

--- backend/app/Models/User/Bookmark.php
+++ backend/app/Models/User/Bookmark.php
@@ -2,9 +2,19 @@
 
 namespace App\Models\User;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Bookmark extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'target_type',
+        'target_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
