93d6aaa661eb4000116896aeb1a0ba64e6eb2361
fayad sh <fayadsh33.fs@gmail.com>
2026-03-12 01:08:04 +0300

    feat(api): enhance reel resource with custom response fields

--- backend/app/Http/Resources/Reel/ReelResource.php
+++ backend/app/Http/Resources/Reel/ReelResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Reel;
 
+use App\Http\Resources\User\UserPreviewResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,18 @@ class ReelResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'caption' => $this->caption,
+            'video_url' => $this->video_url,
+            'slug' => $this->slug,
+            'is_published' => $this->is_published,
+            'total_likes' => 0,
+            'total_shares' => 0,
+            'music' => $this->music,
+            'thumbnail_url' => $this->thumbnail_url,
+            'settings' => $this->setting,
+            'user' => new UserPreviewResource($this->user)
+        ];
     }
 }
