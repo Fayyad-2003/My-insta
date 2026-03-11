9e9a82ea4b6b60754959e132a20d124b6f62bcd7
fayad sh <fayadsh33.fs@gmail.com>
2026-03-12 01:05:16 +0300

    feat(api): enhance post resource with custom response fields

--- backend/app/Http/Resources/Post/PostResource.php
+++ backend/app/Http/Resources/Post/PostResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Post;
 
+use App\Http\Resources\User\UserPreviewResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,16 @@ class PostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'content' => $this->content,
+            'media' => $this->media,
+            'slug' => $this->slug,
+            'is_published' => $this->is_published,
+            'total_likes' => 0,
+            'total_shares' => 0,
+            'settings' => $this->setting,
+            'user' => new UserPreviewResource($this->user)
+        ];
     }
 }
